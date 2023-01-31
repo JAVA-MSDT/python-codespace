4405614f86e83d105ae7824f21968c86b8d02ddc
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-31 12:02:14 +0000

    Adding basics of Python

---
+++ app.py
@@ -0,0 +1,13 @@
+print("Python 3.0")
+print("Python 3.1")
+#-----------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for license information.
+#-----------------------------------------------------------------------------------------
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return app.send_static_file("index.html")
